f004792fa25e410400897ce1b3d29fe7c8c7ec23
Taylor Robbins <trobbins@hiddenbutler.com>
2017-06-20 19:30:40 -0700

    Release 1.1. Added simple line marks when you click on a line number or by pressing Ctrl+M. Added a status bar printout for time elapsed since last line receive. Added the COM port name to the window title. Removed max window size limit. Removed TOPMOST window option when in release mode.

--- code/app_version.h
+++ code/app_version.h
@@ -2,8 +2,8 @@
 #define _APP_VERSION_H
 
 #define APP_VERSION_MAJOR    1
-#define APP_VERSION_MINOR    0
+#define APP_VERSION_MINOR    2
 
-#define APP_VERSION_BUILD    76
+#define APP_VERSION_BUILD    1
 
 #endif // _APP_VERSION_H
