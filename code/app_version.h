a3fc175686330cac42bcb13b67fefd9d9cb91e2d
Taylor Robbins <trobbins@hiddenbutler.com>
2017-08-24 21:13:13 -0700

    Const Port 1.4 release!

--- code/app_version.h
+++ code/app_version.h
@@ -2,8 +2,8 @@
 #define _APP_VERSION_H
 
 #define APP_VERSION_MAJOR    1
-#define APP_VERSION_MINOR    3
+#define APP_VERSION_MINOR    4
 
-#define APP_VERSION_BUILD    926
+#define APP_VERSION_BUILD    3
 
 #endif // _APP_VERSION_H
