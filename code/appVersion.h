d857f3d97b3321c9a83be703ccde189f5aef9fba
Taylor Robbins <trobbins@sure-fi.com>
2017-10-12 08:16:58 -0600

    Fix git's stupid merge stuff

--- code/appVersion.h
+++ code/appVersion.h
@@ -4,10 +4,6 @@
 #define APP_VERSION_MAJOR    1
 #define APP_VERSION_MINOR    5
 
-<<<<<<< HEAD
-#define APP_VERSION_BUILD    959
-=======
-#define APP_VERSION_BUILD    950
->>>>>>> 33d1a91000f037b23644e129ed892bfd4da19032
+#define APP_VERSION_BUILD    961
 
 #endif // _APP_VERSION_H
